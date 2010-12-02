5183d329159e6278be793f1212cb5f35a83c1358
Christopher Klein <ckl@notebook.(none)>
2010-12-02 22:44:36 +0100

    implemented functionality

--- download.js
+++ download.js
@@ -1,119 +1,178 @@
 
+// simple queue system
+function Queue() {
+	this.buffer = new Array();
+	this.__size = 0;
+
+	this.push = function(obj) {
+		this.buffer.push(obj);
+		this.__size++;
+	}
+
+	// removes object from filesLeft and invalidates
+	this.invalidate = function(obj) {
+		// dont blame me, I know the performance problem. The filesLeft should use some sort of ring buffer or hashing
+		for (var i = 0, m = this.buffer.length; i < m; i++) {
+			if (this.buffer[i] == obj) {
+				this.buffer[i] = undefined;
+				this.buffer.slice(i);
+				this.__size--;
+				break;
+			}
+		}
+	}
+
+	this.size = function() {
+		return this.__size;
+	}
+}
+
 var jsdom = require("jsdom"), 
 	window = jsdom.jsdom().createWindow(),
 	http = require('http'),
 	path = require('path'),
 	qs = require("querystring"),
 	fs = require('fs'),
-	document = "",
-	file = "dump.txt",
-	file2 = "res.txt",
-	host = "www.americanhardcorebook.com",
-	subPath = "/punk24",
-	url = host + subPath,
-	array = new Array(),
-	workers = new Array(),
-	maxWorkers = 10;
+	fileMirror = "local.mirror",
+	fileDatabase = "local.database",
+	targetHost = "www.americanhardcorebook.com",
+	targetPath = "/punk24/",
+	targetUrl = targetHost + targetPath,
+	filesLeft = new Array(),
+	workers = new Queue(),
+	maxWorkers = 10,
+	seperator = "|||",
+	working = true,
+	isInitalized = false,
+	workerId = 0,
+	currentWorkers = 0;
 
+function createWorker() {
+	var worker = http.createClient(80, targetHost);
+	var obj = filesLeft.pop();
 
-setTimeout(function() {
-	if (workers.length < maxWorkers && array.length > 0) {
-		console.log("Creating new Worker");
-		worker = http.createClient(80, host);
-		var bla = array.pop();
-		var r = qs.unescape(bla[1].split("?f=")[1]);
+	workers.push(worker);	
+	worker.id = ++workerId;
+	console.log("Creating new Worker [" + worker.id + "] for file [" + obj.file + "] ...");
 
-		request = worker.request('GET', r, {'host': host});
-		request.end();
-		request.on('response', function(res) {
-			var i = workers.length;
-			var data;
-			var file  = bla[0] + ".mp3";
-			var stream = fs.createWriteStream(bla[0] + ".mp3");
-			res.on('data', function(chunk) {
-				console.log("Chunk [Worker] [" + i + "] received");
-				if (stream.writeable) {
-					stream.write(chunk);
-				}
-			});
-	
-			res.on('end', function() {
-				stream.end();
-			});
+	var request = worker.request('GET', obj.path, {'host': targetHost});
+	request.end();
+	request.on('response', function(res) {
+
+	var stream = fs.createWriteStream(obj.file);
+		res.on('data', function(chunk) {
+			if (stream.writeable) {
+				stream.write(chunk);
+			}
 		});
 
-		workers.push(worker);			
+		res.on('end', function() {
+			console.log("Finished download of " + obj.file);	
+			stream.end();
+			request = undefined;
+			workers.invalidate(worker);
+		});
+	});
+}
+
+function checkWorker() {
+	console.log("check worker [current workers: " + workers.size() + ", unfinished files: " + filesLeft.length + "]");
+
+	while (workers.size() < maxWorkers && filesLeft.length > 0) {
+		createWorker();
 	}
-}, 10);
+};
 
-function parseData(file2) {
-
-	fs.readFile(file2, "utf-8", function(err, data) {
+// reads the local database, structure is file $sep path
+function parseDatabase() {
+	fs.readFile(fileDatabase, "utf-8", function(err, data) {
 		var lines = data.split("\n");
+		// remove last (empty) line
 		lines.pop();
+		isInitalized = true;
+
 		lines.forEach(function(line) {
-			var data = line.split("|||");
-			var filename = data[0]; 
-			path.exists(filename + ".mp3", function(exists) {
+			var row = line.split(seperator), 
+			    // decode URI
+			    obj = {'file' : row[0].replace(/[\/|\$]/g, "_") + ".mp3", 'path': qs.unescape(row[1].split("?f=")[1])};
+
+			path.exists(obj.file, function(exists) {
 				if (!exists) {
-					array.push(new Array(data[0], data[1]));
+					console.log("File [" + obj.file + "] does not exist. Move to filesLeft.");
+					filesLeft.push(obj);
 				}
 			});
 		});
 	});
 }
 
-function parseSourceFile(file) {
-	console.log("parsing " + file);
-	fs.readFile(file, "utf-8", function(err, data) {
-		document += data;
+function parseHtmlSite() {
+	console.log("parsing local mirror file [" + fileMirror + "]");
+
+	fs.readFile(fileMirror, "utf-8", function(err, data) {
 		jsdom.jQueryify(window, 'http://code.jquery.com/jquery-1.4.2.min.js', function() {
-			window.document.innerHTML = document;
-			var download = "";
+			window.document.innerHTML = data;
+			var databaseContent = "";
+			// search for #data table and iterate over each row
 			window.$('table.data > tr').each(function(elem) {
 				jq = window.jQuery;
 				jq(this).each(function() {
+					// find all columns
 					var $td = jq(this).find("td");
 					var url = jq($td[0]).find("a").attr("href");
 					var titel = jq.trim(jq($td[0]).text());
 					var artist = jq.trim(jq($td[1]).text());
 					if (url != undefined && url.length > 0) {
-						download += artist + " - " + titel + "|||" + url + "\n";
+						databaseContent += artist + " - " + titel + seperator + url + "\n";
 					}
 				});
 			});
-			fs.writeFile(file2, download);
-			parseData(file2);
+			fs.writeFile(fileDatabase, databaseContent);
+			parseDatabase();
 		});
 	});
 }
 
-path.exists(file2, function(exists) {
-	if (exists) {
-		parseData(file2);
-	} else {
-		path.exists(file, function(exists) {
-			if (!exists) {
-				client = http.createClient(80, host);
-				request = client.request('GET', subPath, {'host': host}),
-				request.end();
-				request.on('response', function(res) {
-					var i = 0;
+function init() {
+	console.log("Initalizing...");
+	path.exists(fileDatabase, function(exists) {
+		if (exists) {
+			parseDatabase();
+		} else {
+			path.exists(fileMirror, function(exists) {
+				if (!exists) {
+					var htmlContent = "";
+					var client = http.createClient(80, targetHost);
+						console.log("Request goes to " + targetHost + targetPath);
 
-					res.on('data', function(chunk) {
-						console.log("Chunk [" + i + "] received");
-						document += chunk;
-						i++;
-					});
+					var request = client.request('GET', targetPath, {'host': targetHost});
+					request.end();
+					request.on('response', function(res) {
+						var i = 0;
+					
+						if (res.statusCode != 200) {
+							console.log("Server sends no response HTTP 200 [was: " + res.statusCode + "]");
+							return;
+						}
+
+						res.on('data', function(chunk) {
+							console.log("Chunk [" + i + "] received from [" + targetHost + "]");
+							htmlContent += chunk;
+							i++;
+						});
 	
-					res.on('end', function() {
-						fs.writeFile(file, document);
-						parseSourceFile(file);
+						res.on('end', function() {
+							fs.writeFile(fileMirror, htmlContent);
+							parseHtmlSite();
+						});
 					});
-				});
-			} else {
-				parseSourceFile(file);
-			}
-		});
-	}
-});
+				} else {
+					parseHtmlSite();
+				}
+			});
+		}
+	});
+}
+
+init();
+setInterval(checkWorker, 2000);
